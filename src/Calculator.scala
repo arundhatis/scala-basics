3ead96bf232eb8a04fc3ec1005ac3b351c55dcfa
Arundhati Sawant <arundhati.u.sawant@gmail.com>
2017-02-05 13:15:08 +0530

    Refactoring to use functions for operations

--- src/Calculator.scala
+++ src/Calculator.scala
@@ -1,10 +1,15 @@
 class Calculator {
-  def calculate(op1: Int, op2: Int, operand: String) = {
-    operand match {
-      case "add" => op1 + op2
-      case "subtract" => op1 - op2
-      case "multiply" => op1 * op2
-      case "divide" => op1 / op2
+  val add: (Int, Int) => Int = _ + _
+  val subtract: (Int, Int) => Int = _ - _
+  val multiply: (Int, Int) => Int= _*_
+  val divide: (Int, Int) => Int = _ / _
+
+  def calculate(op1: Int, op2: Int, operator: String) = {
+    operator match {
+      case "add" => add(op1, op2)
+      case "subtract" => subtract(op1, op2)
+      case "multiply" => multiply(op1, op2)
+      case "divide" => divide(op1, op2)
       case _ => -1
     }
   }
